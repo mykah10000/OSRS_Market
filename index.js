8dc66461d072f6d9694f80f9dcdc385e6b9df8dd
mykah10000 <micahluck8@gmail.com>
2025-12-01 16:42:18 -0500

    First commit for OSRS.Market frontend

---
+++ index.js
@@ -0,0 +1,137 @@
+const params = new URLSearchParams(window.location.search);
+let currentPage = params.get("page") || 1;
+let search = params.get("search") || "";
+let filterOrder = "asc";
+let filter = "id";
+let itemsLength = 0;
+
+
+document.getElementById("search").addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+        search = document.getElementById("search").value.trim();
+        loadItems(1,search);
+    }
+})
+
+document.getElementById("nextButton").addEventListener("click", () => {
+    if (currentPage*50<itemsLength){
+        currentPage++;
+        loadItems(currentPage,search);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+})
+
+document.getElementById("prevButton").addEventListener("click", () => {
+    if (currentPage>1){
+        currentPage--;
+        loadItems(currentPage,search);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+})
+
+document.getElementById("priceFilter").addEventListener("click", () => {
+    if (filter == "price"){
+        filterOrder = filterOrder === "desc" ? "asc" : "desc";
+        if(filterOrder == "asc"){
+            document.getElementById("priceFilter").innerHTML = `Price△`;
+        }
+        else{
+            document.getElementById("priceFilter").innerHTML = `Price▽`;
+        }
+    }
+    else{
+        filterOrder = "asc"
+        resetFilters();
+        document.getElementById("priceFilter").innerHTML = `Price△`;
+        filter = "price";
+    }
+    loadItems(currentPage,search,filter,filterOrder);
+})
+
+document.getElementById("nameFilter").addEventListener("click", () => {
+    if (filter == "name"){
+        filterOrder = filterOrder === "desc" ? "asc" : "desc";
+        if(filterOrder == "asc"){
+        document.getElementById("nameFilter").innerHTML = `Name△`;
+        }
+        else{
+            document.getElementById("nameFilter").innerHTML = `Name▽`;
+        }
+    }
+    else{
+        filterOrder = "asc"
+        resetFilters();
+        document.getElementById("nameFilter").innerHTML = `Name△`;
+        filter = "name";
+    }
+    loadItems(currentPage,search,filter,filterOrder);
+})
+
+document.getElementById("updatedFilter").addEventListener("click", () => {
+    if (filter == "updated"){
+        filterOrder = filterOrder === "desc" ? "asc" : "desc";
+        if (filterOrder == "asc"){
+            document.getElementById("updatedFilter").innerHTML = `Updated△`;
+        }
+        else{
+            document.getElementById("updatedFilter").innerHTML = `Updated▽`;
+        }
+    }
+    else{
+        filterOrder = "asc"
+        resetFilters();
+        document.getElementById("updatedFilter").innerHTML = `Updated△`;
+        filter = "updated";
+    }
+    loadItems(currentPage,search,filter,filterOrder);
+})
+
+document.getElementById("volumeFilter").addEventListener("click", () => {
+    if (filter == "volume"){
+        filterOrder = filterOrder === "desc" ? "asc" : "desc";
+        if (filterOrder == "asc"){
+            document.getElementById("volumeFilter").innerHTML = `Volume△`;
+        }
+        else{
+            document.getElementById("volumeFilter").innerHTML = `Volume▽`;
+        }
+    }
+    else{
+        filterOrder = "asc"
+        resetFilters();
+        document.getElementById("volumeFilter").innerHTML = `Volume△`;
+        filter = "volume";
+    }
+    loadItems(currentPage,search,filter,filterOrder);
+})
+
+async function resetFilters(){
+    document.getElementById("priceFilter").innerHTML = `Price`;
+    document.getElementById("nameFilter").innerHTML = `Name`;
+    document.getElementById("updatedFilter").innerHTML = `Updated`;
+    document.getElementById("volumeFilter").innerHTML = `Volume`;
+}
+async function loadItems(page = 1, searchContents = "") {
+            // const response = await fetch(`/api/items?page=${page}&search=${searchContents}`);
+            const response = await fetch(`http://127.0.0.1:3000/api/items?page=${page}&search=${searchContents}&filter=${filter}&filterOrder=${filterOrder}`);
+            const data = await response.json();
+            itemsLength = data.total;
+            const container = document.getElementById("items");
+            container.innerHTML = "";
+
+            (data.items).forEach(item => {
+                container.innerHTML += `
+                    <div class="item">
+                        <a href="http://127.0.0.1:3000/item?item_id=${item.item_id}"><strong>${item.item_name}</strong></a><br>
+                        $${item.price}
+                    </div>
+                `;
+            });
+        }
+loadItems(currentPage,search,filter,filterOrder);
---
+++ item.js
@@ -0,0 +1,22 @@
+const params = new URLSearchParams(window.location.search);
+const itemId = params.get("item_id") || 2;
+async function loadImage(item_id = 2){
+    document.getElementById("image").src = `https://services.runescape.com/m=itemdb_rs/obj_sprite.gif?id=${item_id}`;
+}
+async function loadItems(item_id = 2){
+    const response = await fetch(`http://127.0.0.1:3000/api/item_details?item_id=${item_id}`);
+    const data = await response.json();
+    const details = data[0];
+    document.getElementById("item_limit").innerHTML = `Item Limit is: ${details.item_limit}`;
+    document.getElementById("high_alch").innerHTML = `High Alch: ${details.high_alch}`;
+    document.getElementById("low_alch").innerHTML = `Low Alch: ${details.low_alch}`;
+    document.getElementById("item_name").innerHTML = `${details.item_name}`;
+    document.getElementById("item_examine").innerHTML = `Examine: ${details.item_examine}`;
+    document.getElementById("item_id").innerHTML = `Item Id: ${details.item_id}`;
+    document.getElementById("members").innerHTML = `Is Members: ${details.members}`;
+    document.getElementById("price").innerHTML = `Price: ${details.price}`;
+    document.getElementById("volume").innerHTML = `Volume: ${details.volume}`;
+    document.getElementById("updated_at").innerHTML = `Updated At: ${details.updated_at}`;
+}
+loadImage(itemId);
+loadItems(itemId);
